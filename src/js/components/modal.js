f8b6e6126d2fd93d8f978b6ceff1d6fab56c71c2
Вячеслав Хохлов <slava071172@mail.ru>
2023-03-30 19:45:10 +0300

    kommit

--- src/js/components/modal.js
+++
@@ -1,8 +0,0 @@
-// M O D A L  CLOSE
-let contactsModalClose = document.querySelector('.contacts-modal__close');
-let contactsModal = document.querySelector('.contacts-modal');
-
-contactsModalClose.addEventListener('click', function(){
-	contactsModalClose.classList.toggle('active');
-	contactsModal.classList.toggle('active');
-})
